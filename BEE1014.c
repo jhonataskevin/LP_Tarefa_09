0d954837def969c73e219ebb41827ce3da6c64a6
Jhonatas Kévin <jhonataskevin552@gmail.com>
2024-10-14 13:38:25 -0300

    Create BEE1014.c

---
+++ BEE1014.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  	int X;
+  	float Y, medio;
+  	
+  
+  	scanf("%d", &X);
+  	
+  
+  	scanf("%f", &Y);
+  	
+  	medio = X / Y;
+  	
+  	printf("%.3f km/l\n ", medio);
+    
+	return 0;
+}
